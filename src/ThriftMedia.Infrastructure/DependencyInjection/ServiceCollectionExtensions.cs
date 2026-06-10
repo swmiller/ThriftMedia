c16fc5ae8c5061a2d3cbb9a83471d95e76104eae
Stephen Miller <smiller@crowninformationservices.com>
2026-06-10 14:55:42 -0500

    Removed obsolete repositories.

--- src/ThriftMedia.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ src/ThriftMedia.Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,12 +1,5 @@
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using ThriftMedia.Application.Repositories;
-using ThriftMedia.Application.Services;
-using ThriftMedia.Domain.Services;
-//using ThriftMedia.Infrastructure.Persistence.Models;
-using ThriftMedia.Infrastructure.Repositories;
-using ThriftMedia.Infrastructure.Services;
 
 namespace ThriftMedia.Infrastructure.DependencyInjection;
 
@@ -14,26 +7,7 @@ public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("ThriftMediaDb")
-            ?? throw new InvalidOperationException("Connection string 'ThriftMediaDb' not found.");
-
-        //services.AddDbContext<ThriftMediaDbContext>(options => options.UseSqlServer(connectionString));
-
-        // Register generic repository
-        //services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-
-        // Register specific repositories
-        //services.AddScoped<IMediaRepository, MediaRepository>();
-        //services.AddScoped<IStoreRepository, StoreRepository>();
-
-        // Register domain services
-        //services.AddScoped<IMediaModerationService, MediaModerationService>();
-
-        // Register application services
-        //services.AddScoped<IOcrService, OcrService>();
-        //services.AddScoped<IMediaClassificationService, MediaClassificationService>();
-        //services.AddScoped<IContentModerationService, ContentModerationService>();
-
+        // TODO: determine if this file is still necessary.
         return services;
     }
 }
